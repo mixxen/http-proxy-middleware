46a49c2b4987c6efaef72b1bc6db4560f7fd6314
chimurai <stevenchim@gmail.com>
2017-04-09 19:48:13 +0200

    fix lint warnings

--- test/lint.js
+++ test/lint.js
@@ -1,21 +1,24 @@
-var exec = require('child_process').exec;
-
-var majorVersion = parseInt(process.versions.node[0], 10);
-var command = '"node_modules/.bin/eslint" --config=.eslintrc.json --format=codeframe index.js lib examples --color';
-
-// Skip eslint on older node versions: < node@4.0.0
+// Skip ESLint on older node versions: < node@4.0.0
 // https://travis-ci.org/chimurai/http-proxy-middleware/builds/212791414
+
+var exec = require('child_process').exec;
+var command = '"node_modules/.bin/eslint" --config=.eslintrc.json --format=codeframe index.js lib examples --color';
+var majorVersion = parseInt(process.versions.node[0], 10);
 
 if (majorVersion > 0) {
     exec(command, execHandler);
 } else {
-    console.log('Skipping ESLint on older Node versions');
+    console.log('ESLint: Skipping ESLint on older Node versions');
 }
 
 function execHandler(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
     if (error !== null) {
-        console.log('exec error: ' + error);
+        console.log('ESLint exec error: ' + error);
+    }
+
+    if (stdout) {
+        console.log(stdout);
+    } else {
+        console.info('ESLint: No linting issues found.');
     }
 }
